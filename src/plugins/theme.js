8ffc686d01273d823534b2bf8d41de72fad9b47f
Manuel <manuel.frick@oysterlab.ch>
2020-03-15 16:18:31 +0100

    add theme file to specify colors later, pre-fill with red

---
+++ src/plugins/theme.js
@@ -0,0 +1,13 @@
+export default {
+  themes: {
+    light: {
+      primary: "#f44336",
+      secondary: "#607d8b",
+      accent: "#2196f3",
+      error: "#ff9800",
+      warning: "#ff5722",
+      info: "#03a9f4",
+      success: "#8bc34a"
+    }
+  }
+}
