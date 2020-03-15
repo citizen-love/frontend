12ed52d7013d869498ecf34a48a7c7cfdeb67a5f
Till <tillmann.felippi@gmail.com>
2020-03-15 01:52:44 +0100

    added dynamic "help needs" list on the index page (incl. test data as object), added a super simple "help selector" navigation component

--- src/views/Index.vue
+++ src/views/Index.vue
@@ -1,40 +1,88 @@
+<!-- we should utilize the skeleton loader methods from vuetify: https://vuetifyjs.com/en/components/skeleton-loaders/ -->
+
 <template>
   <div>
-    <v-row class="">
-      <v-col cols="12">
-        <h1>Citizen help citizen</h1>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet animi architecto beatae, consequatur cum eum
-          expedita facilis hic magnam minima minus provident, quas quos similique soluta tempora voluptates
-          voluptatibus. Omnis.</p>
-      </v-col>
-    </v-row>
-    <v-row class="">
-      <v-col cols="6">
-        <v-card hover>
-          <v-card-title>I need help!</v-card-title>
-          <v-card-subtitle>groceries, babysitting, children sitting</v-card-subtitle>
-        </v-card>
-      </v-col>
-      <v-col cols="6">
-        <v-card hover>
-          <v-card-title>I can help!</v-card-title>
-          <v-card-subtitle>groceries, babysitting, children sitting</v-card-subtitle>
-        </v-card>
-      </v-col>
-    </v-row>
+    <helptypeNavigation></helptypeNavigation>
     <v-row>
       <v-col cols="12">
-        //Todo: cards interactive & dynamic content</v-col>
+        <v-card
+          v-for="entry in helpNeeds"
+          v-bind:key="entry.id"
+          v-bind:title="entry.title"
+          v-bind:details="entry.details"
+          v-bind:category="entry.category"
+          v-bind:zipcode="entry.zipcode"
+          v-bind:city="entry.city"
+          v-bind:latlng="entry.latlng"
+          v-bind:helpcount="entry.helpcount"
+        >
+          <v-list-item>
+            <v-list-item-content>
+              <v-list-item-title>{{ entry.title }}</v-list-item-title>
+              <v-list-item-subtitle>{{ entry.category }} in {{ entry.zipcode }} {{ entry.city }}</v-list-item-subtitle>
+              <v-card-text>{{ entry.details }}</v-card-text>
+            </v-list-item-content>
+
+            <v-list-item-avatar tile size="80" color="grey"></v-list-item-avatar>
+          </v-list-item>
+          <v-card-actions>
+            <v-btn outlined small>Hilfe anbieten ({{entry.helpcount}})</v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-col>
     </v-row>
   </div>
 </template>
 
 <script>
-  export default {
-    name: "Index.vue"
+import helptypeNavigation from "@/components/helptypeNavigation.vue";
+
+export default {
+  name: "Index",
+  components: {
+    helptypeNavigation
+  },
+  data: function() {
+    return {
+      helpNeeds: [
+        {
+          id: 1,
+          title: "Hilfe beim Wäschewaschen",
+          details:
+            "I'm baby swag 8-bit shaman mlkshk sustainable, poutine blog cloud bread fanny live-edge af XOXO roof party hoodie. ",
+          category: "Hausarbeit",
+          zipcode: "8004",
+          city: "Zurich",
+          latlng: "47.378594, 8.510398",
+          helpcount: 3
+        },
+        {
+          id: 2,
+          title: "Brauche jemanden der für mich Einkaufen geht",
+          details:
+            "nackwave kombucha viral roof party art party pop-up yuccie activated charcoal celiac enamel pin street art tote bag thundercats before they sold out church-key",
+          category: "Einläufe erledigen",
+          zipcode: "8004",
+          city: "Zurich",
+          latlng: "47.378594, 8.510398",
+          helpcount: 3
+        },
+        {
+          id: 3,
+          title: "Babysitting an Nachmittagen",
+          details:
+            " Man bun DIY migas normcore chambray chartreuse try-hard yuccie hammock.",
+          category: "Kinderbetreuung",
+          zipcode: "8004",
+          city: "Zurich",
+          latlng: "47.378594, 8.510398",
+          helpcount: 3
+        }
+      ]
+    };
   }
+};
 </script>
 
 <style scoped>
-
 </style>
