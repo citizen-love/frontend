c685a8b165f38b67a38410b411ff995486eb6870
Manuel Frick <manuel.frick@elinica.ch>
2020-03-19 21:12:03 +0100

    add firebase config to repo

---
+++ src/firebase/apiConfig.js
@@ -0,0 +1,11 @@
+
+export const config = {
+  apiKey: "AIzaSyD15_90dAw9ZAtnojq61vlTWyyPoW6dx-c",
+  authDomain: "citizen-love.firebaseapp.com",
+  databaseURL: "https://citizen-love.firebaseio.com",
+  projectId: "citizen-love",
+  storageBucket: "citizen-love.appspot.com",
+  messagingSenderId: "335381408480",
+  appId: "1:335381408480:web:e7aca6929867bdededfe85",
+  measurementId: "G-CFJHGML1MH"
+}
