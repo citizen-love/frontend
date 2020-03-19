64c295c9ffce46531c00ddd3c55f8c22e8b98ba7
Manuel Frick <manuel.frick@elinica.ch>
2020-03-19 23:46:59 +0100

    remove console statement

--- src/services/HelpRequestService.js
+++ src/services/HelpRequestService.js
@@ -6,7 +6,7 @@ import { helpRequestCollection} from "../firebase/firebaseConfig"
     return helpRequestCollection.get().then(function(querySnapshot) {
       querySnapshot.forEach(function(doc) {
         // doc.data() is never undefined for query doc snapshots
-        console.log(doc.id, " => ", doc.data());
+        return doc;
       });
     });
   }
