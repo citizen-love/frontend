0854c2f435b06f96ce327b50fb55076873f94b9a
Manuel Frick <manuel.frick@elinica.ch>
2020-03-27 00:52:05 +0100

    use browser language as default language

--- src/plugins/i18n.js
+++ src/plugins/i18n.js
@@ -16,8 +16,13 @@ function loadLocaleMessages () {
   return messages
 }
 
+function getBrowserLanguage(){
+  return navigator.language.split('-')[0];
+}
+
+
 export default new VueI18n({
-  locale: process.env.VUE_APP_I18N_LOCALE || 'en',
-  fallbackLocale: process.env.VUE_APP_I18N_FALLBACK_LOCALE || 'en',
+  locale: getBrowserLanguage(),
+  fallbackLocale: 'en',
   messages: loadLocaleMessages()
 })
